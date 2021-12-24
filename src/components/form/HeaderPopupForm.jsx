fe13b30198c88a8e90d0cde472452b3c2b37f91f
Naveh Mevorach <navehmevorach@gmail.com>
2021-12-24 11:56:23 +0200

    changes

--- src/components/form/HeaderPopupForm.jsx
+++ src/components/form/HeaderPopupForm.jsx
@@ -33,69 +33,70 @@ const HeaderPopupForm = () => {
   }
 
   return (
-    <>
-      <form id="contact-form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="messages"></div>
-        <div className="row controls">
-          <div className="col-12">
-            <div className="input-group-meta form-group mb-20">
-              <label>Name</label>
-              <input
-                autoComplete="off"
-                type="text"
-                placeholder="Your Name"
-                name="name"
-                {...register("name")}
-                className={`${errors.name ? "is-invalid" : ""}`}
-              />
-              {errors.name && (
-                <div className="invalid-feedback">{errors.name?.message}</div>
-              )}
-            </div>
-          </div>
-          {/* End .col */}
-
-          <div className="col-12">
-            <div className="input-group-meta form-group mb-20">
-              <label>Email</label>
-              <input
-                autoComplete="off"
-                placeholder="Email Address"
-                name="email"
-                type="text"
-                {...register("email")}
-                className={` ${errors.email ? "is-invalid" : ""}`}
-              />
-              {errors.email && (
-                <div className="invalid-feedback">{errors.email?.message}</div>
-              )}
-            </div>
-          </div>
-          {/* End .col */}
-
-          <div className="col-12">
-            <div className="input-group-meta form-group mb-30">
-              <label>Escooter / Ebike / Bike</label>
-              <input
-                autoComplete="off"
-                type="text"
-                placeholder="Your Model"
-                name="model"
-                {...register("name")}
-                className={`${errors.name ? "is-invalid" : ""}`}
-              />
-              {errors.model && (
-                <div className="invalid-feedback">{errors.model?.message}</div>
-              )}
-            </div>
-          </div>
-
-          <div className="col-12">
-            <button className="theme-btn-seven w-100">Send</button>
+    <form
+      className="font-gordita"
+      id="contact-form"
+      onSubmit={handleSubmit(onSubmit)}>
+      <div className="messages"></div>
+      <div className="row controls">
+        <div className="col-12">
+          <div className="input-group-meta form-group mb-20">
+            <label>Name</label>
+            <input
+              autoComplete="off"
+              type="text"
+              placeholder="Your Name"
+              name="name"
+              {...register("name")}
+              className={`${errors.name ? "is-invalid" : ""}`}
+            />
+            {errors.name && (
+              <div className="invalid-feedback">{errors.name?.message}</div>
+            )}
           </div>
         </div>
-      </form>
-    </>
+        {/* End .col */}
+
+        <div className="col-12">
+          <div className="input-group-meta form-group mb-20">
+            <label>Email</label>
+            <input
+              autoComplete="off"
+              placeholder="Email Address"
+              name="email"
+              type="text"
+              {...register("email")}
+              className={` ${errors.email ? "is-invalid" : ""}`}
+            />
+            {errors.email && (
+              <div className="invalid-feedback">{errors.email?.message}</div>
+            )}
+          </div>
+        </div>
+        {/* End .col */}
+
+        <div className="col-12">
+          <div className="input-group-meta form-group mb-30">
+            <label>Escooter / Ebike / Bike</label>
+            <input
+              autoComplete="off"
+              type="text"
+              placeholder="Your Model"
+              name="model"
+              {...register("name")}
+              className={`${errors.name ? "is-invalid" : ""}`}
+            />
+            {errors.model && (
+              <div className="invalid-feedback">{errors.model?.message}</div>
+            )}
+          </div>
+        </div>
+
+        <div className="col-12">
+          <button className="theme-btn-seven demo-button w-100">Send</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
